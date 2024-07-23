d5ebeda1fe673fe643104e384ad4b13516ec27e2
Manju Prem <50079772+manjuprem12@users.noreply.github.com>
2024-07-23 23:04:26 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,7 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        teal: {
-          400: '#4fd1c5',
-          500: '#38b2ac',
-        },
-      },
-      zIndex: {
-        '100': '100',
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
